0212fde8787ef63879e16f6b7ccd7db3d43840d5
boxtamods <boxtafox24@gmail.com>
2025-01-27 14:26:25 +0300

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,11 @@
+local defaultNotePos = {};
+ 
+function onSongStart()
+    for i = 0,7 do 
+        x = getPropertyFromGroup('strumLineNotes', i, 'x')
+ 
+        y = getPropertyFromGroup('strumLineNotes', i, 'y')
+ 
+        table.insert(defaultNotePos, {x,y})
+    end
+end
